d505e250772485a8deb2cc70f6b6c419e1159296
César Alexandre <cesaralexandre@outlook.com>
2023-03-11 21:02:26 -0300

    Code comments / Docstring as comments.

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# Course Name: Python 3 from Basic to Advanced
+# Instructor: Otávio Miranda
+# Teaching Platform: Udemy
+# Student: César Alexandre
+# Lesson Title: Code comments / Docstring as comments.
+# Date: March 11, 2023
+
+
+# is used to comment a single line
+
+'''
+Docstring can be used to comment multiple lines
+'''
+
+"""
+It can be either single quotes or double quotes
+"""
+
+# Note: Unlike the "#" symbol, Python interprets the content within a docstring.
