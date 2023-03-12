6d1a209f1500c3a94805a30d1b2637260408de61
César Alexandre <cesaralexandre@outlook.com>
2023-03-11 21:13:44 -0300

    The function print.

---
+++ lesson2.py
@@ -0,0 +1,23 @@
+# Course Name: Python 3 from Basic to Advanced
+# Instructor: Otávio Miranda
+# Teaching Platform: Udemy
+# Student: César Alexandre
+# Lesson Title: The function print.
+# Date: March 11, 2023
+
+
+#code for disabling the softspace feature
+print('G','F','G', sep='')
+ 
+#for formatting a date
+print('09','12','2016', sep='-')
+ 
+#another example
+print('pratik','geeksforgeeks', sep='@')
+
+# ends the output with a space
+print("Welcome to", end = ' ')
+print("GeeksforGeeks", end= ' ')
+
+# \r\n -> CRLF
+# \n -> LF
