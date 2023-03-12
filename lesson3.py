35c1bdc5532adc67de1af3ed2e00b6b685652100
César Alexandre <cesaralexandre@outlook.com>
2023-03-11 21:31:58 -0300

    Type str (string) - Introduction to types for data.

---
+++ lesson3.py
@@ -0,0 +1,29 @@
+# Course Name: Python 3 from Basic to Advanced
+# Instructor: Otávio Miranda
+# Teaching Platform: Udemy
+# Student: César Alexandre
+# Lesson Title: Type str (string) - Introduction to types for data.
+# Date: March 11, 2023
+
+
+'''
+Python = Programming language
+Type of typing = Dynamic / Strong
+str -> string -> text
+Strings are texts that are enclosed in quotes.
+'''
+
+# Single Quptes
+print('César Alexandre')
+
+# Double Quotes
+print("César Alexandre")
+
+# Escape
+print('César \'Alexandre\'')
+
+# r
+print(r'César \'Alexandre\'')
+
+# An ideal way to include quotes within a string is to use both single and double quotes.
+print('César "Alexandre"')
